8581e4883b31a5d3e7278155904c571b0e1925ec
s1F102100445 <s1F102100445@iniad.org>
2023-12-06 15:46:11 +0900

    index2.htmlを追加(他キャラのページ)
    index.htmlにindex2.htmlに飛ぶリンクも追加

--- mental/urls.py
+++ mental/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('index2/', views.index2, name='index2'),
     path('index_view/', views.index_view, name='index_view'),
+    path('index_view2/', views.index_view2, name='index_view2')
 ]
