e4a0603cb9463ac7ef9ec43912742b94b0d317da
s1F102103156 <s1F102103156@iniad.org>
2024-01-14 18:57:40 +0900

    login_and_register

--- mental/views.py
+++ mental/views.py
@@ -1,6 +1,6 @@
 import requests
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
