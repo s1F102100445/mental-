3ed91d4dd40ed4bc480c90274f4e907ed818b928
s1F102100445 <s1F102100445@iniad.org>
2024-01-16 21:28:01 +0900

    s

--- build.sh
+++ build.sh
@@ -3,11 +3,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-pip install requests
-pip install openai
-pip install --upgrade pip
-pip install pipwin
-pipwin install pyaudio
-pip install PyAudio-0.2.11-cp39-cp39-win_amd64.whl
+
 python manage.py collectstatic --no-input
 python manage.py migrate
