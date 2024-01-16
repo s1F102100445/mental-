f58f91b42aad4ec2070dc31537c3f4bb2cfc0142
s1F102100445 <s1F102100445@iniad.org>
2024-01-16 21:11:04 +0900

    pip upgrade

--- build.sh
+++ build.sh
@@ -5,9 +5,7 @@ set -o errexit
 pip install -r requirements.txt
 pip install requests
 pip install openai
-
+pip install --upgrade pip
 pip install PyAudio-0.2.11-cp39-cp39-win_amd64.whl
-pip install pipwin
-pipwin install pyaudio
 python manage.py collectstatic --no-input
 python manage.py migrate
