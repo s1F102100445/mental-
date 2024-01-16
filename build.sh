66abcd60df0e7c09878e41a985d63c716539754a
s1F102103156 <s1F102103156@iniad.org>
2024-01-16 23:12:01 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,7 @@
 set -o errexit
 
 pip install --upgrade pip
-sudo apt-get install portaudio19-dev
 pip install pyaudio
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py superuser
