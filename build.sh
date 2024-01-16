4ad5dd62a5b5bbaa9dd2ba8ea57f08a977889c03
s1F102103156 <s1F102103156@iniad.org>
2024-01-16 22:52:20 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
 pip install --upgrade pip
-
+sudo apt-get install portaudio19-dev
+pip install pyaudio
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py superuser
