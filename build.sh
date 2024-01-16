455a48f7f64d2e46f4b6cdc4d7e7cf3c09dd0b94
s1F102100445 <s1F102100445@iniad.org>
2024-01-16 21:50:37 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 pip install -r requirements.txt
 pip install --upgrade pip
-pip install PyAudio-0.2.11-cp39-cp39-win_amd64.whl
+sudo apt-get install portaudio19-dev
+pip install pyaudio
 python manage.py collectstatic --no-input
 python manage.py migrate
