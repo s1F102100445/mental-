5db1b5acc1eee57fa0ac25d10f5f57eba5737727
s1F102103156 <s1F102103156@iniad.org>
2024-01-16 22:17:06 +0900

    render

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-pip install --upgrade pip
-sudo apt-get install portaudio19-dev
-pip install pyaudio
+
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py superuser
