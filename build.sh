cc7cb3e465c7f78f15b81d82a5bbf929ca649fab
s1F102100445 <s1F102100445@iniad.org>
2024-01-16 22:21:19 +0900

    test

--- build.sh
+++ build.sh
@@ -4,7 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 pip install --upgrade pip
-sudo apt-get install portaudio19-dev
-pip install pyaudio
 python manage.py collectstatic --no-input
 python manage.py migrate
